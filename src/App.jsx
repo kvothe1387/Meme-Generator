606c598abb58e2ae696bad846af5e4456627998b
David McCullough <dmccullough488@gmail.com>
2025-03-10 00:41:05 -0500

    *

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Header from "./components/Header"
-import Main from "./components/Main"
-import "./index.css"
+import Header from "./components/Header";
+import Main from "./Main";
+import "./index.css";
 
 export default function App() {
 
